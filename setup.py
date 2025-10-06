097c4b7533b1c1a7dda0b0e07820a252d2b5cd2c
Jon Durbin <jon@jondurbin.com>
2025-10-06 10:47:16 -0400

    First commit.

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md")) as infile:
+    long_description = infile.read()
+
+setup(
+    name="cllmv",
+    version="0.0.1",
+    description="Chutes fast LLM verification.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chutesai/cllmv",
+    author="chutes.ai",
+    license_expression="MIT",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    package_data={
+        "cllmv": ["lib/*.so"],
+    },
+    install_requires=[
+        "setuptools>=0.75",
+    ],
+    extras_require={
+        "dev": [
+            "ruff",
+            "wheel",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+)
---
+++ src/cllmv/__init__.py
@@ -0,0 +1,27 @@
+from cllmv.base import ChutesLLMVerifier
+
+_verifier = None
+
+
+def init():
+    global _verifier
+    _verifier = ChutesLLMVerifier()
+
+
+def generate(id: str, created: int, value: str) -> str:
+    if _verifier is None:
+        init()
+    return _verifier.generate(id, created, value)
+
+
+def validate(id: str, created: int, value: str, expected_hash: str, salt: str) -> bool:
+    if _verifier is None:
+        init()
+    return _verifier.validate(id, created, value, expected_hash, salt)
+
+
+__all__ = [
+    "ChutesLLMVerifier",
+    "validate",
+    "generate",
+]
